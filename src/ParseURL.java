35e0bbf8ace10ceb3465e182ab874b4c59436580
seet61 <dmitry.arefyev@RST-BSS-041-NB.Megafon.ru>
2016-11-13 12:42:28 +0300

    example for URL

--- src/ParseURL.java
+++ src/ParseURL.java
@@ -11,5 +11,12 @@ public class ParseURL {
                 + "/index.html?name=networking#DOWNLOADING");
 
         System.out.println("protocol = " + aURL.getProtocol());
+        System.out.println("authority = " + aURL.getAuthority());
+        System.out.println("host = " + aURL.getHost());
+        System.out.println("port = " + aURL.getPort());
+        System.out.println("path = " + aURL.getPath());
+        System.out.println("query = " + aURL.getQuery());
+        System.out.println("filename = " + aURL.getFile());
+        System.out.println("ref = " + aURL.getRef());
     }
 }
